7f5e39faabe33d40147dde0839a20ab74307c46e
zhang-ning <hunter.dding@gmail.com>
2014-01-11 22:18:14 +0800

    serial spec and feature implemented

--- test/serial.js
+++ test/serial.js
@@ -1,28 +1,59 @@
-var seq = require('../');
+var serial = require('../').serial;
+var assert = require('assert');
 
 describe('serial case',function(){
+  var first,second,third,result='no call';
   beforeEach(function(done){
-    function first(promise){
+    first = function (){
+      var me = this;
       setTimeout(function(){
-        promise.reject();
-        promise.resolve();
+        result = 'first called';
+        me.done();
       },500);
-    }
-    function second(){
+    };
+    second = function (){
+      var me = this;
       setTimeout(function(){
-
+        result = 'second called';
+        me.done();
       },500);
-    }
-    function third(){
+    };
+    third = function (){
+      var me = this;
       setTimeout(function(){
-
+        result = 'third called';
+        me.done();
       },500);
-    }
+    };
+    var queue = new serial();
+    queue.run(first).then(second).then(third);
+    done();
+  });
 
-    seq.serial(first).success(second).success(third);
-    seq.serial(first).fail(second).fail(third);
-    seq.serial(first).then(second).then(third);
-    seq.serial(first).route['customize'](second).then(third);
+  it('#no call till 500ms',function(){
+    assert.equal('no call',result);
+  });
+
+  it('#first called after 500ms',function(done){
+    setTimeout(function(){
+      assert.equal('first called',result);
+      done();
+    },500);
+  });
+
+  it('#second called after 1000ms',function(done){
+    setTimeout(function(){
+      assert.equal('second called',result);
+      done();
+    },1000);
+  });
+
+
+  it('#third called after 1500ms',function(done){
+    setTimeout(function(){
+      assert.equal('third called',result);
+      done();
+    },1500);
   });
 });
 
