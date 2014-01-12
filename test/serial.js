2ee02824de66e449d14bde6fb3ff18e38f644597
zhang-ning <hunter.dding@gmail.com>
2014-01-12 13:53:32 +0800

    parallel

--- test/serial.js
+++ test/serial.js
@@ -2,60 +2,41 @@ var serial = require('../').serial;
 var assert = require('assert');
 
 describe('serial case',function(){
-  var first,second,third,result='no call';
-  beforeEach(function(done){
-    first = function (){
-      var me = this;
-      setTimeout(function(){
-        result = 'first called';
-        me.done();
-      },500);
+  var task1,task2,task3,data=[];
+  before(function(done){
+    task1 = function (){
+      setTimeout(function () {
+          data.push({num:'task1'});
+          queue.next('task1 successed',200);
+      }, 50);
     };
-    second = function (){
-      var me = this;
-      setTimeout(function(){
-        result = 'second called';
-        console.log(me);
-        me.done();
-      },500);
+    task2 = function (msg,code){
+      var args = arguments;
+      setTimeout(function () {
+        data.push({num:'task2',args:args});
+        queue.next('task2 failed',500,function(){});
+      }, 50);
     };
-    third = function (){
-      var me = this;
-      setTimeout(function(){
-        result = 'third called';
-        console.log(123123);
-        me.done();
-      },500);
+    task3 = function (msg,code,fn){
+      var args = arguments;
+      setTimeout(function () {
+        data.push({num:'task3',args:args});
+        done();
+      }, 100);
     };
-    var queue = new serial();
-    queue.run(first).then(second).then(third);
-    done();
+
+    queue = new serial();
+    queue.push(task1);
+    queue.push(task2);
+    queue.push(task3);
+    queue.exec();
   });
 
-  it('#no call till 500ms',function(){
-    assert.equal('no call',result);
+  it('#data.length should equal 3',function(){
+    assert(3 === data.length);
   });
-
-  it('#first called after 500ms',function(done){
-    setTimeout(function(){
-      assert.equal('first called',result);
-      done();
-    },600);
-  });
-
-  it('#second called after 1000ms',function(done){
-    setTimeout(function(){
-      assert.equal('second called',result);
-      done();
-    },1100);
-  });
-
-
-  it('#third called after 1500ms',function(done){
-    setTimeout(function(){
-      assert.equal('third called',result);
-      done();
-    },1600);
+  it('#task3 argumets[2] should equal 500',function(){
+    assert.equal(500,data[2].args[1]);
   });
 });
 
